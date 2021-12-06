ef758e4200b7978282d0f6f9e8dd51950cee7663
Kristy Tsang <kristytsang1@gmail.com>
2021-12-05 23:37:56 -0700

    github authentication

--- components/authentication/providers/github/GitHubProvider.js
+++ components/authentication/providers/github/GitHubProvider.js
@@ -1,10 +1,33 @@
 import Image from "next/image";
-
 import { ProviderButton } from "ui/buttons";
-
+import { useRouter } from 'next/dist/client/router'
+import { GithubAuthProvider, signInWithPopup } from '@firebase/auth';
+import { useState } from 'react'
+import { useAuth } from 'lib/hooks/useAuth';
+import { auth } from 'lib/firebase';
 import github from "./github.png";
 
 function GitHubProvider({ children, ...props }) {
+
+  const router = useRouter()
+  const user = useAuth()
+  const [isValidUser, setIsValidUser] = useState(null)
+  const provider = new GitHubAuthProvider()
+
+  // request a signin with a pop up window -> pass the provider
+  async function requestLogin () {
+    setIsValidUser(await signInWithPopup(auth, provider))
+  }
+
+  // click event passing to a custom function
+  function handleClick () {
+    // sign in with github
+    requestLogin()
+  }
+
+  if(isValidUser) {
+    router.push('/todo')
+  }
   return (
     <ProviderButton>
       <div>
