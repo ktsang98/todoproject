80e6fb2f83300e820fd44b3af9e339ce337b5662
Kristy Tsang <kristytsang1@gmail.com>
2021-12-17 12:27:01 -0700

    new files and content folders

---
+++ pages/api/add-item/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { AddNewItem, ItemID, AddItem, ItemDescription } from "./styles";
+import {AppBar} from 'components/appbar'
+
+import { Brand } from "components/brand";
+ 
+function AddNewItemPage({...props}) {
+ 
+
+  return (
+    <>
+       
+    </>
+  );
+}
+
+export default AddNewItemPage;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,39 @@
-import { AppBar } from "../components/appbar"
-import { GoogleProvider, GitHubProvider } from "../components/authentication/providers"
-import { Title } from "../ui/title"
-import { Spacer } from "../ui/spacers"
-import {UserLogin} from "../components/authentication/user-login"
-import { Legal, HighLight } from "../ui/legal"
-import {PageLayout, PageHeader, PageFooter, PageBody} from '../layouts/loginpage'
-function index (){
-    
-    return(
-        <>
-              <AppBar/>
-              <PageLayout>
-                    <PageHeader>
-                    <Title>Account Login</Title>
-                    </PageHeader>
-               <PageBody>
-                   <GoogleProvider style={{marginBottom:"1.5rem"}}>With Google</GoogleProvider>
-                   <GitHubProvider>With Github</GitHubProvider>
-                   <Spacer className="spacing" >OR</Spacer>
-                   <UserLogin/>
-                </PageBody>             
-                <PageFooter>
-                        <Legal>Legal Stuff  <HighLight>terms and conditions</HighLight></Legal>
-                </PageFooter>
+import Link from 'next/link'
+import styled from 'styled-components';
 
-              </PageLayout>
-        </>
-    )
+const LinksContainer = styled.div`
+width: 50%;
+margin: 2rem auto;
+display: flex;
+justify-content: center;
+
+a {
+  color: crimson;
+  padding: 2rem;
 }
 
- 
 
-export default index
+`;
+
+function index(props) {
+
+
+
+return (
+  <>
+    <header>
+      <h1>To Do List App</h1>
+      <LinksContainer>
+        <Link href="/login">
+          <a>login</a>
+        </Link>
+        <Link href="/login">
+          <a>sign up</a>
+        </Link>
+      </LinksContainer>
+    </header>
+  </>
+  );
+}
+
+export default index;
